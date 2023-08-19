4f35e7151cb6644e19f092c98e630e01064a7867
Gayathri Devi Kuppusamy <gayathri.kuppusamy@itp.objectfrontier.com>
2023-08-19 10:27:51 +0530

    First commit

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,10 @@
 class Myclass{
+ void method1()
+   {
+    System.out.println("Hello!");
+  }
   public static void main(String []args){
- 
+    Myclass obj =new Myclass();
+    obj.method1();
   }
 }
